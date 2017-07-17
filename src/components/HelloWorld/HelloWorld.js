3860325aba31aaa97a74d82a31b079db6b4eaa3f
jtwd <james@jtwd.co.uk>
2017-07-17 17:01:19 +0100

    Modified scripts so the pre compile doesnt happen for docs on normal start

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { string } from 'prop-types'
 
+/** Component that says 'Hello' to the world */
 function HelloWorld({message = 'World'}) {
   return <div>Hello {message}</div>
 }
 
 HelloWorld.propTypes = {
+  /** Message to be displayed */
   message: string
 }
 

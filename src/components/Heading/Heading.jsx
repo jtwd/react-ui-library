d53e9ad81509c59059379ce4297afa256f485c75
jtwd <james@jtwd.co.uk>
2017-07-26 11:44:49 +0100

    Updated switch statement in Heading to have a default case and pass linting

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -6,14 +6,14 @@ import { H2, H3, H4, H5 } from './Heading.styles'
 function Heading({level, children}) {
   if (!children) return null
   switch(level) {
-    case 1:
-      return <H2>{children}</H2>
     case 2:
       return <H3>{children}</H3>
     case 3:
       return <H4>{children}</H4>
     case 4:
       return <H5>{children}</H5>
+    default:
+      return <H2>{children}</H2>
   }
 }
 

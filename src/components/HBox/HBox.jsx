cca1874e94f4603689c0caa9db5516abfb4f7ebf
jtwd <james@jtwd.co.uk>
2017-07-31 13:23:18 +0100

    HBox component added

---
+++ src/components/HBox/HBox.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { node, oneOf } from 'prop-types'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  
+  ${props => props.align === 'top' && `
+    align-items: flex-start;
+  `}
+  ${props => props.align === 'bottom' && `
+    align-items: flex-end;
+  `}
+  
+  ${props => props.pack === 'start' && `
+    justify-content: flex-start;
+  `}
+  ${props => props.pack === 'center' && `
+    justify-content: center;
+  `}
+  ${props => props.pack === 'end' && `
+    justify-content: flex-end;
+  `}
+  ${props => props.pack === 'space-between' && `
+    justify-content: space-between;
+  `}
+  ${props => props.pack === 'space-around' && `
+    justify-content: space-around;
+  `}
+`
+
+/** Flexbox layout helper for horizontal alignment */
+function HBox({ children, align, pack, ...props}) {
+  return (
+    <StyledDiv align={align} pack={pack} {...props}>{children}</StyledDiv>
+  )
+}
+
+HBox.propTypes = {
+  align: oneOf(['stretch', 'top', 'bottom']),
+  pack: oneOf(['start', 'center', 'end', 'space-between', 'spacer-around', null]),
+  children: node.isRequired
+}
+
+HBox.defaultProps = {
+  align: 'stretch',
+  pack: null
+}
+
+export default HBox

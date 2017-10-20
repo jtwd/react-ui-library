1e9b2d7c113a3d7f636924d648e236c32042afd1
James Taylor <james.t@jamest.core.MMTDigital>
2017-10-20 09:27:56 +0100

    Height for select box

--- src/components/SelectBox/SelectBox.styles.js
+++ src/components/SelectBox/SelectBox.styles.js
@@ -7,10 +7,10 @@ export const StyledSelectBox = styled.select`
   ${props => getInputStyles(props.error)}
   padding-top: .46rem;
   padding-bottom: .46rem;
-  height: 36px;
+  height: 35.5px;
   ${media.xs`height: 38px;`}
-  ${media.lg`height: 42px;`}
-  ${media.wide`height: 47px;`}
+  ${media.lg`height: 47px;`}
+  ${media.wide`height: 51px;`}
 `
 
 export const Option = styled.option`
